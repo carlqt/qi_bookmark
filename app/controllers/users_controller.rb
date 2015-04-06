73f12e044acbdbb494a759b8903422d048fcd919
Carl Tablante <carl.q.tablante@gmail.com>
2015-04-06 17:16:06 +0800

    added bootstrap tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
   def index
-    @links = Link.includes(:user)
+    @link = Link.new
+    @shared_links = Link.includes(:user)
+  end
+
+  def create
+    if params[:commit] == 'Post'
+      @link = Link.new link_params
+
+      if @link.save
+        redirect_to :back
+      else
+        render :index
+      end
+
+    elsif params[:commit] == 'Share'
+      
+    end
+
+    binding.pry
+    'lol'
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:name)
   end
 end
