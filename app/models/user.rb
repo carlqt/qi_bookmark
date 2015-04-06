12d27eb7ec2cd4d4b9f544f42e31832075ea286a
Carl Tablante <carl.q.tablante@gmail.com>
2015-04-06 16:38:58 +0800

    added a form for posting and sharing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
 
   has_many :relationships
   has_many :friends, through: :relationships
+  has_many :links
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406080816) do
+ActiveRecord::Schema.define(version: 20150406082535) do
+
+  create_table "links", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150406080816) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
