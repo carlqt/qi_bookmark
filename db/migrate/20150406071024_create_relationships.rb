43a7ee8d5e3e915c8d9ca6975ff36927654c7c6f
Carl Tablante <carl.q.tablante@gmail.com>
2015-04-06 15:58:55 +0800

    fixed navbar

--- db/migrate/20150406071024_create_relationships.rb
+++ db/migrate/20150406071024_create_relationships.rb
@@ -3,7 +3,6 @@ class CreateRelationships < ActiveRecord::Migration
     create_table :relationships do |t|
       t.integer :user_id
       t.integer :friend_id
-      t.string :type
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406071024) do
+ActiveRecord::Schema.define(version: 20150406074733) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150406071024) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
