5ec4246159f2edec11f6ebece08644d4b4f2ff14
Carl Tablante <carl.q.tablante@gmail.com>
2015-04-07 23:20:45 +0800

    added authenticate user in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @link = Link.new
     @shared_links = Link.viewable(current_user)
